ef393523518c2bb27655eebea39048aad8184859
mohan lal sahu <mohansahu18111811@gmailcom>
2024-12-09 10:18:03 +0530

    routes

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createCategory, getAllCategories, getCategoryById, deleteCategory, updateCategory } from "../controllers/category.controller.js";
-import { isAdmin } from "../middlewares/auth.middleware.js";
+import { auth, isAdmin } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.get("/get", getAllCategories);
 router.get("/get/:id", getCategoryById)
 
+// router.use(auth)
 // router.use(isAdmin)
 
 router.post("/create", createCategory);
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,16 +1,17 @@
 
 import { Router } from "express";
 import { createProduct, deleteProduct, getAllProducts, updateProduct, getProductById, deleteSingleImage, deleteMultipleImages } from "../controllers/product.controller.js";
-import { isAdmin } from "../middlewares/auth.middleware.js";
+import { auth, isAdmin } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.get("/get", getAllProducts);
 router.get("/get/:id", getProductById)
-router.delete("/delete-single-image/", deleteSingleImage)
-router.delete("/delete-multiple-images/", deleteMultipleImages)
 
 // router.use(isAdmin)
+// router.use(auth)
 
+router.delete("/delete-single-image/", deleteSingleImage)
+router.delete("/delete-multiple-images/", deleteMultipleImages)
 router.put("/update/:id", updateProduct)
 router.post("/create", createProduct);
 router.delete("/delete/:id", deleteProduct)
