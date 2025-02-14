f59b9ae8becd7c3e82d9a18161176f3dad37472f
Dhanjeet09 <dhanjeetsharma7@gmail.com>
2025-02-14 16:18:26 +0530

    feat: add CustomerDetails page and integrate routing in admin section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,6 +39,7 @@ import Team from "./pages/admin/team/Team";
 import Products from "./pages/admin/products/Products";
 import Admin from "./pages/admin/Admin";
 import OrderDetails from "./pages/admin/orders/OrderDetails";
+import CustomerDetails from "./pages/admin/customers/CustomerDetails";
 
 // Utility Functions
 const getRoutesConfig = () => ({
@@ -66,6 +67,7 @@ const getRoutesConfig = () => ({
   adminRoutes: [
     { path: "add-product", element: <AddNewProduct /> },
     { path: "customers", element: <Customers /> },
+    { path: "customers/:id", element: <CustomerDetails /> },
     { path: "orders", element: <Orders /> },
     { path: "orders/:id", element: <OrderDetails /> },
     { path: "team", element: <Team /> },
