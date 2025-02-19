c4d4741a7aeb3505b28240b6892d0d3e2258e239
Mohit Mehto <imohitmehto@gmail.com>
2025-02-19 15:31:18 +0530

    refactor: update payment processing functions for improved clarity and functionality

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,16 +1,12 @@
 import express from "express";
-import {
-  handlePayment,
-  verifyPaymentStatus,
-  // getAllPaymentData,
-} from "../controllers/payment.controller.js";
+import { createPayment, handlePaymentResponse } from "../controllers/payment.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
 router.use(auth);
 
-router.post("/pay", handlePayment);
-router.post("/status/:paymentOrderId", verifyPaymentStatus);
-// router.get("/info", getAllPaymentData);
+router.post("/create-payment", createPayment);
+router.post("/payment-response", handlePaymentResponse);
 
 export default router;
