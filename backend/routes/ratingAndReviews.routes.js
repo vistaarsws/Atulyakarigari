6c8a2c4a940eb0d5e193101bfd0951eb4223fd7f
Dhanjeet09 <dhanjeetsharma7@gmail.com>
2025-03-06 17:28:27 +0530

    Refactor rating and reviews routes and improve review fetching logic in Product component

--- backend/routes/ratingAndReviews.routes.js
+++ backend/routes/ratingAndReviews.routes.js
@@ -8,12 +8,13 @@ import { auth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+router.get("/getAll/:productId", getReviewsByProduct); // Retrieves all reviews for a product
+
 // Apply authentication middleware to all routes
 router.use(auth);
 
 // Routes
 router.post("/createOrUpdate", createOrUpdateReview); // Handles both create and update
-router.get("/getAll/:productId", getReviewsByProduct); // Retrieves all reviews for a product
 router.delete("/delete/:id", deleteReview); // Deletes a review
 
 export default router;
