2325d92a90a8d42bafc6b488e610b3aa560f3bdf
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2025-03-07 14:42:42 +0530

    navbar search fix

--- frontend/src/pages/user/view-cart/Index.jsx
+++ frontend/src/pages/user/view-cart/Index.jsx
@@ -30,14 +30,17 @@ const Index = () => {
   localStorage.setItem("orderData", JSON.stringify(orderData));
 
   return (
-    <Box sx={{ pt: { sm: "0" } }}>
+    <Box sx={{ pt: { sm: "0" }, height: "90vh", outline: "2px solid green" }}>
       <Stepper />
 
       {cartData.items?.length > 0 ? (
         <Box
           sx={{
             marginTop: "2rem",
-            height: "100%",
+            height: "80vh",
+            overflowY: "hidden",
+            scrollbarWidth: "none",
+            boxShadow: "none",
             display: "flex",
             justifyContent: "space-around",
             flexDirection: { xs: "column", md: "row" },
@@ -54,7 +57,16 @@ const Index = () => {
           >
             <OrderCard cartData={cartData} />
           </Box>
-          <Box sx={{ width: { xs: "100%", md: "35%", marginBottom: "7rem" } }}>
+          <Box
+            sx={{
+              width: {
+                xs: "100%",
+                md: "35%",
+                height: "83vh",
+                overflowY: "scroll",
+              },
+            }}
+          >
             <Payment orderData={orderData} />
           </Box>
         </Box>
